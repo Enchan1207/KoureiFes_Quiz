714fbab0eee693ddeccd6b0617bf7c75196c169e
Enchan <enchant0130@gmail.com>
2021-10-11 23:07:20 +0900

    [Update #6] multiple entries

--- assets/scripts/manager.js
+++ assets/scripts/manager.js
@@ -1,13 +1,9 @@
-// assets/app.js
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+//
+// assets/manager.js
+//
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../styles/manager.scss';
 
 // Need jQuery? Install it with "yarn add jquery"(or "npm install jquery"), then uncomment to import it.
 // import $ from 'jquery';
---
+++ assets/scripts/viewer.js
@@ -0,0 +1,12 @@
+//
+// assets/viewer.js
+//
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../styles/viewer.scss';
+
+// Need jQuery? Install it with "yarn add jquery"(or "npm install jquery"), then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
+
