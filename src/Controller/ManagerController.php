c1b3daf01d9bf34af29eb8434656dbf8fdc0d6e1
Enchan <enchant0130@gmail.com>
2021-10-12 10:03:46 +0900

    [Add #7] Manager-Controller

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Puzzle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class ManagerController extends AbstractController
     #[Route('/manager', name: 'manager')]
     public function index(): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Puzzle::class);
+        $puzzles = $repository->findAll();
+
         return $this->render('manager/index.html.twig', [
             'controller_name' => 'ManagerController',
+            'puzzles' => $puzzles
         ]);
     }
 }
