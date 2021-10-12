fd089eb8ae18a868990fcfd6fb326b4e25b456af
Enchan <enchant0130@gmail.com>
2021-10-12 18:02:19 +0900

    [Add #12] Puzzle list command

---
+++ src/Command/PuzzleListCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Puzzle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use function PHPSTORM_META\type;
+
+#[AsCommand(
+    name: 'puzzle:list',
+    description: 'list up all Puzzles',
+)]
+class PuzzleListCommand extends Command
+{
+
+    private $puzzleRepository;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->puzzleRepository = $container->get('doctrine')->getRepository(Puzzle::class);
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title("Puzzle List:");
+
+        $puzzles = $this->puzzleRepository->findAll();
+        
+        foreach ($puzzles as $puzzle) {
+            $answer = implode(",", $puzzle->getAnswer());
+            $stringRepresentedPuzzle = "ID:{$puzzle->getId()} {$puzzle->getWidth()}×{$puzzle->getHeight()} ({$answer}) -> {$puzzle->getReward()}";
+            $io->text($stringRepresentedPuzzle);
+        }
+
+        $io->info("To add new Puzzle, exec `console puzzle:add`.");
+
+        return Command::SUCCESS;
+    }
+}
