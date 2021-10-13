fd551a6bf1620a43d7c51069efdcaab92db6717d
Enchan <enchant0130@gmail.com>
2021-10-13 18:08:02 +0900

    [Modify #15] script

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,17 +5,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../styles/app.scss';
 
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
+document.addEventListener('load', () => {
 
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-
-window.addEventListener('load', () => {
-    console.log("KoureiFes_Quiz");
-    $('[data-toggle="popover"]').popover();
 });
+
