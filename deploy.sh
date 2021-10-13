d3a546d2a34bfdb93cb87e92f55b529ed9970eea
Enchan <enchant0130@gmail.com>
2021-10-14 08:52:55 +0900

    [Fix #27] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ yes | $PHP $CONSOLE puzzle:init
 
 # パズル追加
 
-echo "2\n2\n3,0,1,2\n#DC143C,#00008B,#FFD700,#228B22\n[ 1  2  3 ]\n" | $PHP $CONSOLE puzzle:add
+echo -e "2\n2\n3,0,1,2\n#DC143C,#00008B,#FFD700,#228B22\n[ 1  2  3 ]\n" | $PHP $CONSOLE puzzle:add
 
-echo "3\n3\n2,4,3,7,6,1,5,0,8\n#444,#444,#444,#444,#444,#444,#444,#444,#444\n[ 1  2  3 ]\n" | $PHP $CONSOLE puzzle:add
+echo -e "3\n3\n2,4,3,7,6,1,5,0,8\n#444,#444,#444,#444,#444,#444,#444,#444,#444\n[ 1  2  3 ]\n" | $PHP $CONSOLE puzzle:add
